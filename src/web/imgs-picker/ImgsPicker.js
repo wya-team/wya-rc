a4db78721ced8a2f86b7a738ee5444c9ef71ee1c
Deot <zrd0921@qq.com>
2018-05-11 08:18:16 +0800

    fix warn

--- src/web/imgs-picker/ImgsPicker.js
+++ src/web/imgs-picker/ImgsPicker.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './ImgsPicker.scss';
-import Upload from '../Upload';
+import Upload from '../upload/index';
 import { getParseUrl } from '../utils/utils';
 class ImgsPicker extends Component {
 	constructor(props, context) {
