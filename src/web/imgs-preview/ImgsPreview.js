f6e8880f31f7e7c03216faece8b7fbc834f7538f
Deot <zrd0921@qq.com>
2018-05-27 19:45:59 +0800

    fix
    莫名奇妙的问题，目前还不知道是怎么造成的

--- src/web/imgs-preview/ImgsPreview.js
+++ src/web/imgs-preview/ImgsPreview.js
@@ -4,7 +4,10 @@ import classnames from 'classnames';
 import { pick } from 'lodash';
 import Core from './Core.js';
 import events from './events';
-// 当遇到Modal里的预览，第二次节点会查找失败，这里不使用this.thumbnails;
+/**
+ * 当遇到Modal里的预览，第二次节点会查找失败，这里不使用this.thumbnails,
+ * 原因不明，测试正常，项目异常, 简直莫名其妙, 也不能设置对象
+ */
 let thumbnails = thumbnails || [];
 class ImgsPreview extends React.Component {
 	static PhotoSwipe = Core;
@@ -39,10 +42,15 @@ class ImgsPreview extends React.Component {
 		e.persist();
 		// e.preventDefault();
 		const getThumbBoundsFn = (index) => {
-			const target = thumbnails[index] || e.target;
-			const pageYScroll = window.pageYOffset || document.documentElement.scrollTop;
-			const rect = target.getBoundingClientRect();
-			return { x: rect.left, y: rect.top + pageYScroll, w: rect.width };
+			try {
+				const target = thumbnails[index] || e.target;
+				const pageYScroll = window.pageYOffset || document.documentElement.scrollTop;
+				const rect = target.getBoundingClientRect();
+				return { x: rect.left, y: rect.top + pageYScroll, w: rect.width };
+			} catch (e) {
+				console.log(e);
+			};
+
 		};
 		const { opts } = this.state;
 		opts.index = index;
@@ -71,16 +79,18 @@ class ImgsPreview extends React.Component {
 					className={classnames(['rc-imgs-preview', className]).trim()}
 					style={{ display: `flex`, flexWrap: `wrap`, ...style }}
 				>
-					{dataSource.map((item, index) => (
-						<div
-							key={index}
-							ref={(node) => this.setRef(node, index)}
-							onClick={(e) => this.handleShow(e, index)}
+					{dataSource.map((item, index) => {
+						return (
+							<div
+								key={index}
+								ref={this.inputRef}
+								onClick={(e) => this.handleShow(e, index)}
 
-						>
-							{renderRow(item)}
-						</div>
-					))}
+							>
+								{renderRow(item)}
+							</div>
+						);
+					})}
 				</div>
 				<Target
 					ref="core"
