b4038981db2e625fdf58ae3ec9889028d775c957
chenchen <729041629@qq.com>
2018-06-22 09:52:54 +0800

    无法被动侦听事件preventDefault
    修复chrome无法被动侦听事件preventDefault的问题

--- src/web/m-touch/MTouch.js
+++ src/web/m-touch/MTouch.js
@@ -120,7 +120,13 @@ class MTouch extends Component {
 			this.moveX = point.pageX;
 			this.moveY = point.pageY;
 		}
-		event.preventDefault();
+		// 判断默认行为是否可以被禁用
+	    if (event.cancelable) {
+	        // 判断默认行为是否已经被禁用
+	        if (!event.defaultPrevented) {
+	            event.preventDefault();
+	        }
+	    }
 	}
 	handleCancel(event) {
 		this.handleEnd();
